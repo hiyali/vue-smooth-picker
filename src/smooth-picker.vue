c650d7141d12c96ee42f3250c7754cf58392e303
Salam Hiyali <hiyali920@gmail.com>
2017-10-31 16:48:56 +0800

    [optimize] mainly optimize trigger timing of the function of get dom rect & comments for variables

--- src/smooth-picker.vue
+++ src/smooth-picker.vue
@@ -39,39 +39,43 @@
     },
     data () {
       return {
-        currentIndexList: this.getInitialCurrentIndexList(),
-        lastCurrentIndexList: [],
-        groupsRectList: new Array(this.data.length),
-        touchOrMouse: {
-          isTouchable: 'ontouchstart' in window,
-          isMouseDown: false
+        currentIndexList: this.getInitialCurrentIndexList(), // save groups's index
+        lastCurrentIndexList: [], // for detect which group's current index if it is changed
+
+        groupsRectList: new Array(this.data.length), // save the dom rect list of this picker's groups
+
+        dragInfo: { // save drag(ing) info
+          isTouchable: 'ontouchstart' in window, // for detect event belongs to touch or mouse
+          isMouseDown: false, // save the status of mouse (touch) is start and it is not end
+
+          isDragging: false, // for detect the status of mouse (touch) is dragging (moving) after isMouseDown or not
+          groupIndex: null, // save which group is dragging now
+          startPageY: null // save the pageY value of mouse (touch) after begin isMouseDown
         },
-        draggingInfo: {
-          isDragging: false,
-          groupIndex: null,
-          startPageY: null
-        },
-        getRectTimeoutId: null
+
+        supInfo: { // supporting for picker usefulness
+          getRectTimeoutId: null, // save timeout id
+          lastStyleDisplay: null, // for detect picker style display if it is changed
+          watchDomObserver: null // for watching this picker dom
+        }
       }
     },
     mounted () {
       this.eventsRegister()
 
-      this.getGroupsRectList()
+      this.supInfo.watchDomObserver = this.createDomObserver()
+      this.supInfo.watchDomObserver.observe(this.$el, { attributes: true })
 
       window.addEventListener('resize', this.safeGetGroupRectList)
     },
-    updated () {
-      this.$nextTick(function () {
-        this.getGroupsRectList()
-      })
-    },
     destroyed () {
+      this.supInfo.watchDomObserver.disconnect()
+
       window.removeEventListener('resize', this.safeGetGroupRectList)
     },
     methods: {
       setGroupData (gIndex, groupData) {
-        this.data[gIndex] = groupData
+        // for current index list
         const iCI = groupData.currentIndex
         let movedIndex = 0
         if (typeof iCI === 'number' && iCI >= 0 && groupData.list && groupData.list.length && iCI <= groupData.list.length - 1) {
@@ -80,7 +84,14 @@
         this.currentIndexList[gIndex] = movedIndex
         this.lastCurrentIndexList = [].concat(this.currentIndexList)
 
-        this.safeGetGroupRectList()
+        // for detect group flex if changed
+        const gF = groupData.flex
+        if (gF && this.data[gIndex].flex !== gF) {
+          this.safeGetGroupRectList()
+        }
+
+        // set group data
+        this.data[gIndex] = groupData
       },
       getInitialCurrentIndexList () {
         return this.data.map((item, index) => {
@@ -91,9 +102,23 @@
           return 0
         })
       },
+      createDomObserver () {
+        return new window.MutationObserver((mutations) => {
+          mutations.forEach((mutation) => {
+            if (mutation.type === 'attributes') {
+              // for get correct rect list after v-show true (when $el style display not none)
+              const elDisplay = this.$el.style.display
+              if (elDisplay !== 'none' && this.supInfo.lastStyleDisplay !== elDisplay) {
+                this.supInfo.lastStyleDisplay = elDisplay
+                this.$nextTick(this.getGroupsRectList())
+              }
+            }
+          })
+        })
+      },
       safeGetGroupRectList () {
-        this.getRectTimeoutId && clearTimeout(this.getRectTimeoutId)
-        this.getRectTimeoutId = setTimeout(() => {
+        this.supInfo.getRectTimeoutId && clearTimeout(this.supInfo.getRectTimeoutId)
+        this.supInfo.getRectTimeoutId = setTimeout(() => {
           this.getGroupsRectList()
         }, 200)
       },
@@ -111,7 +136,7 @@
         }
       },
       addEventsForElement (el) {
-        const _ = this.touchOrMouse.isTouchable
+        const _ = this.dragInfo.isTouchable
         const eventHandlerList = [
           { name: _ ? 'touchstart' : 'mousedown', handler: this.handleStart },
           { name: _ ? 'touchmove' : 'mousemove', handler: this.handleMove },
@@ -143,7 +168,7 @@
         this.correctionCurrentIndex(ev, gIndex)
       },
       getTouchInfo (ev) {
-        return this.touchOrMouse.isTouchable ? ev.changedTouches[0] || ev.touches[0] : ev
+        return this.dragInfo.isTouchable ? ev.changedTouches[0] || ev.touches[0] : ev
       },
       getGroupIndexBelongsEvent (ev) {
         const touchInfo = this.getTouchInfo(ev)
@@ -179,17 +204,17 @@
         }
 
         const touchInfo = this.getTouchInfo(ev)
-        this.draggingInfo.startPageY = touchInfo.pageY
-        if (!this.touchOrMouse.isTouchable) {
-          this.touchOrMouse.isMouseDown = true
+        this.dragInfo.startPageY = touchInfo.pageY
+        if (!this.dragInfo.isTouchable) {
+          this.dragInfo.isMouseDown = true
         }
       },
       handleMove (ev) {
         ev.preventDefault()
         ev.stopPropagation()
 
-        if (this.touchOrMouse.isTouchable || this.touchOrMouse.isMouseDown) {
-          this.draggingInfo.isDragging = true
+        if (this.dragInfo.isTouchable || this.dragInfo.isMouseDown) {
+          this.dragInfo.isDragging = true
           this.setCurrentIndexOnMove(ev)
         }
       },
@@ -197,11 +222,11 @@
         ev.preventDefault()
         ev.stopPropagation()
 
-        if (!this.draggingInfo.isDragging) {
+        if (!this.dragInfo.isDragging) {
           this.handleEventClick(ev)
         }
-        this.draggingInfo.isDragging = false
-        this.touchOrMouse.isMouseDown = false
+        this.dragInfo.isDragging = false
+        this.dragInfo.isMouseDown = false
 
         this.correctionAfterDragging(ev)
       },
@@ -209,36 +234,36 @@
         ev.preventDefault()
         ev.stopPropagation()
 
-        if (this.touchOrMouse.isTouchable || this.touchOrMouse.isMouseDown) {
+        if (this.dragInfo.isTouchable || this.dragInfo.isMouseDown) {
           this.correctionAfterDragging(ev)
-          this.touchOrMouse.isMouseDown = false
-          this.draggingInfo.isDragging = false
+          this.dragInfo.isMouseDown = false
+          this.dragInfo.isDragging = false
         }
       },
       setCurrentIndexOnMove (ev) {
         const touchInfo = this.getTouchInfo(ev)
-        if (this.draggingInfo.groupIndex === null) {
-          this.draggingInfo.groupIndex = this.getGroupIndexBelongsEvent(ev)
+        if (this.dragInfo.groupIndex === null) {
+          this.dragInfo.groupIndex = this.getGroupIndexBelongsEvent(ev)
         }
 
-        const gIndex = this.draggingInfo.groupIndex
+        const gIndex = this.dragInfo.groupIndex
         if (typeof gIndex === 'number' && (this.data[gIndex].divider || !this.data[gIndex].list)) {
           return
         }
 
-        const moveCount = (this.draggingInfo.startPageY - touchInfo.pageY) / 32
+        const moveCount = (this.dragInfo.startPageY - touchInfo.pageY) / 32
         const movedIndex = this.currentIndexList[gIndex] + moveCount
 
         this.$set(this.currentIndexList, gIndex, movedIndex)
 
-        this.draggingInfo.startPageY = touchInfo.pageY
+        this.dragInfo.startPageY = touchInfo.pageY
       },
       correctionAfterDragging (ev) {
-        const gIndex = this.draggingInfo.groupIndex
+        const gIndex = this.dragInfo.groupIndex
         this.correctionCurrentIndex(ev, gIndex)
 
-        this.draggingInfo.groupIndex = null
-        this.draggingInfo.startPageY = null
+        this.dragInfo.groupIndex = null
+        this.dragInfo.startPageY = null
       },
       correctionCurrentIndex (ev, gIndex) {
         setTimeout(() => {
@@ -291,7 +316,7 @@
         const gapCount = this.currentIndexList[gIndex] - iIndex
         if (Math.abs(gapCount) < 4) {
           let rotateStyle = 'transform: rotateX(' + gapCount * 23 + 'deg) translate3d(0, 0, 5.625rem);'
-          if (!this.draggingInfo.isDragging) {
+          if (!this.dragInfo.isDragging) {
             rotateStyle += ' transition: transform 150ms ease-out;'
           }
           return rotateStyle
