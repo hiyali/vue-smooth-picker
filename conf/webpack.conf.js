aeced16bea729a9ef5e44434d122288d38cc98c5
Salam Hiyali <hiyali920@gmail.com>
2019-05-19 10:59:12 +0600

    Upgrade vue-smooth-picker 0.4.2 & Upgrade webpack to v4

--- conf/webpack.conf.js
+++ conf/webpack.conf.js
@@ -1,8 +1,12 @@
-var path = require('path')
-var webpack = require('webpack')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+'use strict'
 
-var exampleName = process.argv[7]
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+const exampleName = process.argv[8]
 if (!exampleName) {
   console.error('Not give example name, you can enter\n' +
     'npm run build/dev product\n' +
@@ -10,138 +14,87 @@ if (!exampleName) {
   return 5
 }
 
-module.exports = {
-  entry: { 'index': './example/' + exampleName },
-  output: {
-    publicPath: '',
-    filename: '[name].js',
-    path: './example/' + exampleName + '/dist'
-  },
-  resolve: {
-    extensions: ['', '.js', '.vue'],
-    fallback: [path.join(__dirname, '../node_modules')],
-    alias: {
-      'src': path.resolve(__dirname, '../src'),
-      'example': path.resolve(__dirname, '../example')
-    }
-  },
-  resolveLoader: {
-    fallback: [path.join(__dirname, '../node_modules')]
-  },
-  module: {
-    preLoaders: [
-      {
-        test: /\.vue$/,
-        loader: 'eslint',
-        include: [
-          path.join(__dirname, '../src'),
-          path.join(__dirname, '../example')
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.js$/,
-        loader: 'eslint',
-        include: [
-          path.join(__dirname, '../src'),
-          path.join(__dirname, '../example')
-        ],
-        exclude: /node_modules|\.min\.js$/
-      }
-    ],
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue'
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        include: [
-          path.join(__dirname, '../src'),
-          path.join(__dirname, '../example')
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url',
-        query: {
-          limit: 2 * 1024,
-          name: 'dist/img/[name].[hash:7].[ext]'
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url',
-        query: {
-          limit: 10000,
-          name: 'dist/fonts/[name].[hash:7].[ext]'
-        }
-      }
-    ]
-  },
-  eslint: {
-    formatter: require('eslint-friendly-formatter')
-  },
-  stylus: {
-    use: [
-      require('poststylus')([ 'autoprefixer', 'rucksack-css' ])
-    ]
-  },
-  vue: {
-    loaders: {
-      css: ExtractTextPlugin.extract("css"),
-      stylus: ExtractTextPlugin.extract('css!stylus')
+module.exports = function (args, options) {
+  const PROD_MODE = options.mode === 'production'
+  return {
+    entry: {
+      'index': path.join(__dirname, '../example/' + exampleName)
     },
-    postcss: [
-      require('autoprefixer')({
-        browsers: ['last 8 versions']
+    output: {
+      publicPath: './',
+      filename: 'index.js',
+      path: path.join(__dirname, '../example/' + exampleName + '/dist')
+    },
+    resolve: {
+      extensions: ['.js', '.vue'],
+      alias: {
+        'example': path.resolve(__dirname, '../example'),
+        'product': path.resolve(__dirname, '../example/product'),
+        'datetime': path.resolve(__dirname, '../example/datetime'),
+        'gender': path.resolve(__dirname, '../example/gender')
+      }
+    },
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.(vue|js)$/,
+          exclude: /node_modules|\.min\.js$/,
+          loader: 'eslint-loader'
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          include: [
+            path.join(__dirname, '../example')
+          ],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.styl(us)?$/,
+          use: [
+            PROD_MODE
+              ? MiniCssExtractPlugin.loader
+              : 'vue-style-loader',
+            { loader: 'css-loader', options: { importLoaders: 1 }},
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [require('autoprefixer')({
+                  'browsers': ['> 4%', 'last 10 versions']
+                })]
+              }
+            },
+            'stylus-loader'
+          ],
+          include: path.join(__dirname, '../example'),
+          exclude: /node_modules/
+        }
+      ]
+    },
+    optimization: {
+      minimize: PROD_MODE,
+      minimizer: [
+        new TerserJSPlugin({}),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'style.css'
       })
     ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      beautify: false,
-      compress: {
-        warnings      : false,
-        sequences     : true,  // join consecutive statemets with the “comma operator”
-        properties    : true,  // optimize property access: a["foo"] → a.foo
-        dead_code     : true,  // discard unreachable code
-        drop_debugger : false,  // discard “debugger” statements
-        drop_console  : false,  // discard “console” statements
-        unsafe        : true, // some unsafe optimizations (see below)
-        conditionals  : true,  // optimize if-s and conditional expressions
-        comparisons   : true,  // optimize comparisons
-        evaluate      : true,  // evaluate constant expressions
-        booleans      : true,  // optimize boolean expressions
-        loops         : true,  // optimize loops
-        unused        : true,  // drop unused variables/functions
-        hoist_funs    : true,  // hoist function declarations
-        hoist_vars    : true, // hoist variable declarations
-        if_return     : true,  // optimize if-s followed by return/continue
-        join_vars     : true,  // join var declarations
-        cascade       : true,  // try to cascade `right` into `left` in sequences
-        side_effects  : true,  // drop side-effect-free statements
-        global_defs   : {},
-        keep_fnames: true
-      },
-      output: {
-        comments: true
-      }
-    }),
-    new ExtractTextPlugin('style.css')
-  ]
+  }
 }
--- example/datetime/index.js
+++ example/datetime/index.js
@@ -4,6 +4,9 @@ import SmoothPicker from 'vue-smooth-picker'
 import 'vue-smooth-picker/dist/css/style.css'
 Vue.use(SmoothPicker)
 
+import { version } from 'vue-smooth-picker'
+console.warn('vue-smooth-picker version', version)
+
 import Example from './index.vue'
 new Vue(
   Object.assign({}, Example, {
