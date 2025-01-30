c165ce3ded4c78e09a382d66187af05f05658145
Salam Hiyali <hiyali920@gmail.com>
2025-01-31 00:40:29 +0800

    update: version to 1.0.0-beta.6

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { App } from 'vue'
 import SmoothPicker from './components/SmoothPicker.vue'
 
-export { SmoothPicker }
+const version = "1.0.0-beta.6"
+export { SmoothPicker, version }
 
 export default {
     install: (app: App) => {
-        app.component('SmoothPicker', SmoothPicker)
+        app.component('smooth-picker', SmoothPicker)
     }
 } 
