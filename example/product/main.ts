6cf07da08286b815acdf9c12e51c3c6cb9052d8b
Salam Hiyali <hiyali920@gmail.com>
2025-01-31 01:22:17 +0800

    update: testing

--- example/product/main.ts
+++ example/product/main.ts
@@ -4,6 +4,8 @@ import 'vue-smooth-picker/style.css'
 
 import App from './App.vue'
 
+console.warn('component name', SmoothPicker.name)
+console.warn('component tag name', SmoothPicker.tag)
 console.warn('vue-smooth-picker version', version)
 
 createApp(App).component(SmoothPicker.name, SmoothPicker).mount('#app') 
